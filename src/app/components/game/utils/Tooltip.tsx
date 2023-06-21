989695b6e48d6c55ae293e3ea9bfa87f1a11bb7c
Abel Majadibodu <Abeljackson33@gmail.com>
2023-06-21 17:16:54 +0200

    donload button fix

--- src/app/components/game/utils/Tooltip.tsx
+++ src/app/components/game/utils/Tooltip.tsx
@@ -6,19 +6,10 @@ interface TooltipProps {
 }
 
 const Tooltip: React.FC<TooltipProps> = ({ children, text }) => {
-  const [showTooltip, setShowTooltip] = useState(false);
-
-  const handleMouseEnter = () => {
-    setShowTooltip(true);
-
-    setTimeout(() => {
-      setShowTooltip(false);
-    }, 5000);
-  };
 
   return (
-    <div className="tooltip" onMouseEnter={handleMouseEnter}>
-      {showTooltip && <div className="tooltip-text">{text}</div>}
+    <div className="tooltip">
+    <div className="tooltip-text">{text}</div>
       {children}
     </div>
   );
